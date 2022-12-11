b146ab11b8ee1867a58625a81894add98595a90b
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2022-12-11 15:36:56 +0400

    not done

---
+++ FluentApi/DataAccess/EFrameworkServer/EFCustomerRepository.cs
@@ -0,0 +1,38 @@
+﻿using FluentApi.Domain.Abstractions;
+using FluentApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentApi.DataAccess.EFrameworkServer
+{
+    public class EFCustomerRepository : ICustomerRepository
+    {
+        public void AddData(Customer data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteData(Customer data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateData(Customer data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FluentApi/DataAccess/EFrameworkServer/EFOrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using FluentApi.Domain.Abstractions;
+using FluentApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentApi.DataAccess.EFrameworkServer
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        public void AddData(Order data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteData(Order data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateData(Order data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
