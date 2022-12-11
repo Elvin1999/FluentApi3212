03c553a50796f8ce889afc51e9afbe2132c0519b
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2022-12-11 17:26:26 +0400

    done

--- FluentApi/ViewModels/MainViewModel.cs
+++ FluentApi/ViewModels/MainViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FluentApi.ViewModels
 {
@@ -27,6 +28,96 @@ namespace FluentApi.ViewModels
 
             AllOrders =new ObservableCollection<Order>(App.DB.OrderRepository.GetAll());
 
+            AddCommand = new RelayCommand((o) =>
+            {
+                if (Customer.Id >= 1)
+                {
+                    var item = App.DB.CustomerRepository
+                    .GetAll()
+                    .FirstOrDefault(c => c.ContactName == Customer.ContactName);
+                    if (item != null)
+                    {
+                        MessageBox.Show("Customer is already exists");
+                    }
+                    else
+                    {
+                        App.DB.CustomerRepository.AddData(Customer); 
+                        AllCustomers = new ObservableCollection<Customer>(App.DB.CustomerRepository.GetAll());
+                        MessageBox.Show($" customer was added successfully");
+                        ClearForm();
+                    }
+                }
+                else
+                {
+                    App.DB.CustomerRepository.AddData(Customer);
+                    AllCustomers = new ObservableCollection<Customer>(App.DB.CustomerRepository.GetAll());
+                    MessageBox.Show($" customer was added successfully");
+                    ClearForm();
+                }
+
+            });
+
+
+            ResetCommand = new RelayCommand((o) =>
+            {
+                ClearForm();
+            });
+
+
+
+            DeleteCustomerCommand = new RelayCommand((o) =>
+            {
+                   App.DB.CustomerRepository.DeleteData(Customer);
+                AllOrders = new ObservableCollection<Order>(App.DB.OrderRepository.GetAll());
+                AllCustomers = new ObservableCollection<Customer>(App.DB.CustomerRepository.GetAll());
+
+            },
+            (p) =>
+            {
+                if(Customer!=null && Customer.Id != 0)
+                {
+                    return true;
+                }
+                return false;
+            });
+
+            OrderNowCommand = new RelayCommand((o) =>
+            {
+                var order = new Order
+                {
+                    OrderDate = DateTime.Now,
+                    CustomerId = Customer.Id
+                };
+
+                App.DB.OrderRepository.AddData(order);
+                AllOrders = new ObservableCollection<Order>(App.DB.OrderRepository.GetAll());
+                MessageBox.Show("Order completed successfully");
+            },
+         (p) =>
+         {
+             if (Customer != null && Customer.Id != 0)
+             {
+                 return true;
+             }
+             return false;
+         });
+
+
+
+            UpdateCommand = new RelayCommand((o) =>
+            {
+                App.DB.CustomerRepository.UpdateData(Customer);
+                MessageBox.Show("Updated successfully");
+                ClearForm();
+            });
+
+
+
+        }
+
+        public void ClearForm()
+        {
+            Customer = new Customer();
         }
 
 
